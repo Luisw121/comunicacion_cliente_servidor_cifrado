49e91ed1cdd52d1f6ae84c83b951e35df4961f77
Luisw121 <lafernandez@elpuig.xeill.net>
2024-04-04 16:29:48 +0200

    chat funcional con encriptación

--- src/Client.java
+++ src/Client.java
@@ -7,35 +7,33 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
         try {
+
             Socket socket = new Socket("localhost", 1234);
 
             ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
 
-            PublicKey serverPublicKey = (PublicKey) inputStream.readObject();
-
             KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
             keyGen.initialize(2048);
-            KeyPair keyPair = keyGen.generateKeyPair();
-
-            outputStream.writeObject(keyPair.getPublic());
+            KeyPair clientKeyPair = keyGen.generateKeyPair();
+            outputStream.writeObject(clientKeyPair.getPublic());
             outputStream.flush();
 
-            Cipher decryptCipher = Cipher.getInstance("RSA");
-            decryptCipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
+            PublicKey serverPublicKey = (PublicKey) inputStream.readObject();
 
+            Cipher decryptCipher = Cipher.getInstance("RSA");
+            decryptCipher.init(Cipher.DECRYPT_MODE, clientKeyPair.getPrivate());
             Cipher encryptCipher = Cipher.getInstance("RSA");
             encryptCipher.init(Cipher.ENCRYPT_MODE, serverPublicKey);
 
-            BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
-
-            // Hilo para leer mensajes del servidor
             Thread readThread = new Thread(() -> {
                 try {
+
                     while (true) {
-                        byte[] encryptedResponse = (byte[]) inputStream.readObject();
-                        String decryptedResponse = new String(decryptCipher.doFinal(encryptedResponse));
-                        System.out.println("Server: " + decryptedResponse);
+
+                        byte[] encryptedMessage = (byte[]) inputStream.readObject();
+                        String decryptedMessage = new String(decryptCipher.doFinal(encryptedMessage));
+                        System.out.println("Server: " + decryptedMessage);
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -43,20 +41,19 @@ public class Client {
             });
             readThread.start();
 
-            // Hilo para enviar mensajes al servidor
-            Thread writeThread = new Thread(() -> {
-                try {
-                    String inputLine;
-                    while ((inputLine = consoleInput.readLine()) != null) {
-                        byte[] encryptedMessage = encryptCipher.doFinal(inputLine.getBytes());
-                        outputStream.writeObject(encryptedMessage);
-                        outputStream.flush();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-            writeThread.start();
+            BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
+
+            String inputLine;
+            while ((inputLine = consoleInput.readLine()) != null) {
+
+                byte[] encryptedMessage = encryptCipher.doFinal(inputLine.getBytes());
+                outputStream.writeObject(encryptedMessage);
+                outputStream.flush();
+            }
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
 
         } catch (Exception e) {
             e.printStackTrace();
--- src/Server.java
+++ src/Server.java
@@ -1,68 +1,67 @@
-import javax.crypto.Cipher;
-import java.net.*;
 import java.io.*;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.PublicKey;
+import java.net.*;
+import java.security.*;
+import javax.crypto.*;
 
 public class Server {
 
     public static void main(String[] args) throws IOException {
         try {
+
             KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
             keyGen.initialize(2048);
-            KeyPair keyPair = keyGen.generateKeyPair();
+            KeyPair serverKeyPair = keyGen.generateKeyPair();
 
             ServerSocket serverSocket = new ServerSocket(1234);
             System.out.println("Server is listening on port 1234");
-
             Socket socket = serverSocket.accept();
             System.out.println("Client connected");
 
             ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-
-            outputStream.writeObject(keyPair.getPublic());
+            outputStream.writeObject(serverKeyPair.getPublic());
             outputStream.flush();
 
-            Cipher decryptCipher = Cipher.getInstance("RSA");
-            decryptCipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
-
             PublicKey clientPublicKey = (PublicKey) inputStream.readObject();
+
+            Cipher decryptCipher = Cipher.getInstance("RSA");
+            decryptCipher.init(Cipher.DECRYPT_MODE, serverKeyPair.getPrivate());
             Cipher encryptCipher = Cipher.getInstance("RSA");
             encryptCipher.init(Cipher.ENCRYPT_MODE, clientPublicKey);
 
-            BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
-
-            // Hilo para leer mensajes del cleinte
             Thread readThread = new Thread(() -> {
                 try {
-                    while (true) {
-                        byte[] encryptedResponse = (byte[]) inputStream.readObject();
-                        String decryptedResponse = new String(decryptCipher.doFinal(encryptedResponse));
-                        System.out.println("Client: " + decryptedResponse);
-                    }
-                }catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-            readThread.start();
 
-            Thread writeThread = new Thread(() -> {
-                try {
-                    String inputLine;
-                    while ((inputLine = consoleInput.readLine()) != null) {
-                        byte[] encryptedMessage = encryptCipher.doFinal(inputLine.getBytes());
-                        outputStream.writeObject(encryptedMessage);
-                        outputStream.flush();
+                    while (true) {
+
+                        byte[] encryptedMessage = (byte[]) inputStream.readObject();
+                        String decryptedMessage = new String(decryptCipher.doFinal(encryptedMessage));
+                        System.out.println("Client: " + decryptedMessage);
+
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             });
-            writeThread.start();
 
-        }catch (Exception e) {
+            readThread.start();
+
+            BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
+            String inputLine;
+            while ((inputLine = consoleInput.readLine()) != null) {
+
+                byte[] encryptedMessage = encryptCipher.doFinal(inputLine.getBytes());
+
+                outputStream.writeObject(encryptedMessage);
+                outputStream.flush();
+            }
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
